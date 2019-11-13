a0345f463d098abdb9aff0f72b6cc82e2ee7c4e0
Mauricio Degregori <degregori2@gmail.com>
2019-11-13 15:14:53 -0600

    now.json

--- weather-guys/src/components/WeatherCall.jsx
+++ weather-guys/src/components/WeatherCall.jsx
@@ -3,14 +3,18 @@ import React, { useState, useEffect, Component } from "react";
 import axios from "axios";
 import WeatherDisplay from "./WeatherDisplay";
 
+import {useCustomHook} from '../hooks/useCustomHook'
+
 const WeatherCall = () => {
     const [weatherData, setWeatherData] = useState({});
     const [weatherLocation, setWeatherLocation] = useState('');
     const [zip, setZip] = useState('')
+
+ 
     
     useEffect(() => {
         axios
-        .get(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=${weatherData},us&appid=96901f1375395bd16951db545b3c226c`) 
+        .get(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=${zip}us&appid=96901f1375395bd16951db545b3c226c`) 
         .then(response => {
             console.log('testing API', response);
             setWeatherData(response.data.main)
@@ -18,16 +22,12 @@ const WeatherCall = () => {
         .catch(err => console.log(err))
       }, [weatherData]);
 
- 
-  
-
-
     const handleChanges = event => {
         setZip(event.target.value)
  
     }
-
-    const fetchZip = () => {
+    const fetchZip = (e) => {
+        e.preventDefault();
         axios
         .get(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=96901f1375395bd16951db545b3c226c`)
         .then( response => {
@@ -36,21 +36,15 @@ const WeatherCall = () => {
         })
         .catch(err => console.log(err));
     }
-  
 
-        // if (this.state.  < 5){
-        //     return (
-        //         <div>
-        //             <h2>Type in your desired zip code</h2>
-        //             <input type='text' value={this.state.zip} onChange={this.handleChanges} />
-        //             <button onClick={this.fetchZip}>Find Weather</button>
-        //         </div>
-        //     )
-        // }
       return (
         <div>
-          <input type='text' value={zip} onChange={handleChanges} />
-          <button onClick={fetchZip}>Find Weather</button>
+            <form onSubmit={fetchZip}>
+                <div>
+                    <input type='text' value={zip} onChange={handleChanges} />
+                    <button type="submit">Find Weather</button>  
+                </div>
+            </form>
           <WeatherDisplay
           data = {weatherData}
           location={weatherLocation}
